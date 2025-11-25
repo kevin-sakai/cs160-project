d55c0cc75ed2bb85d8ac0162b81ef61b2255769f
michael-wsp <michaelsp@berkeley.edu>
2025-11-24 23:26:51 -0800

    Added css files

---
+++ mainstream/src/pages/Notepad.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import './Notepad.css';
+
+export default function Notepad() {
+    return (
+        <div className="notepad">
+            
+        </div>
+    );
+}
