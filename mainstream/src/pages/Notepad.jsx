337ffd9396e125e3b9f487d4778fc17b603c236a
michael-wsp <michaelsp@berkeley.edu>
2025-11-25 15:04:50 -0800

    Basic notepad functionality: write, change page, localStorage persistence

--- mainstream/src/pages/Notepad.jsx
+++ mainstream/src/pages/Notepad.jsx
@@ -1,10 +1,34 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './Notepad.css';
 
 const NUM_TEXT_ROWS = 25;
 
+// localStorage key scheme: mainstream-<pagenum>
+// {date: <date last modified>, text: <note text>}
+
 export default function Notepad() {
-  const [text, setText] = useState("");
+  const [noteText, setNoteText] = useState("");
+  const [notePage, setNotePage] = useState(0);
+  const [noteDate, setNoteDate] = useState("");
+
+  useEffect(() => {
+    const key = "mainstream-" + notePage;
+    const entry = JSON.parse(localStorage.getItem(key));
+    if (entry) {
+      setNoteText(entry.text);
+      setNoteDate(entry.date);
+    }
+  }, [notePage]);
+
+  useEffect(() => {
+    const key = "mainstream-" + notePage;
+    const item = JSON.stringify({
+      date: "1-1-1",
+      text: noteText,
+    });
+    localStorage.setItem(key, item);
+  })
+
   return (
     <div id="notepad-page">
       <div id="title">
@@ -12,11 +36,20 @@ export default function Notepad() {
       </div>
       <div id="notepad-text-entry">
           <textarea
-            value={text}
-            onChange={(e) => setText(e.target.value)}
+            value={noteText}
+            onChange={(e) => setNoteText(e.target.value)}
             placeholder="Start Writing A Note!"
             rows={NUM_TEXT_ROWS}>
           </textarea>
+          <div id="notepad-page-buttons">
+            <button
+              className="notepad-page-button"
+              onClick={() => setNotePage(notePage - 1)}>&#x2190;</button>
+            <button
+              className="notepad-page-button"
+              onClick={() => setNotePage(notePage + 1)}>&#x2192;</button>
+          </div>
+          <h2>Page {notePage}</h2>
         </div>
     </div>
   );
