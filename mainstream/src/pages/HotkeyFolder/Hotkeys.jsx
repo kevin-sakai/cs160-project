af6d08d43ace846533cbe7964c3c62358224db03
Ariel Pho <arielpho@berkeley.edu>
2025-11-25 17:05:31 -0800

    fixed list error

--- mainstream/src/pages/HotkeyFolder/Hotkeys.jsx
+++ mainstream/src/pages/HotkeyFolder/Hotkeys.jsx
@@ -3,31 +3,34 @@ import { SelectComponent } from "./SelectedComponent";
 import './Hotkeys.css';
 export const Hotkey = () => {
     let actions = [
-        {action: "action 1", hotkey: "a"},
-        {action: "action 2", hotkey: "b"},
-        {action: "action 3", hotkey: "c"},
-        {action: "action 4", hotkey: "d"},
+        { action: "action 1", hotkey: "a" },
+        { action: "action 2", hotkey: "b" },
+        { action: "action 3", hotkey: "c" },
+        { action: "action 4", hotkey: "d" },
     ];
 
     const [selectedOption, setSelectedOption] = useState("")
     return (<div>
-        <SelectComponent 
-        options={actions}
-        onChange={(item)=> setSelectedOption(item)}
-        selectedKey={selectedOption}
-        placeholder={"type to search"}
-        />
+
         <h1>Hotkeys</h1>
+
+
+
         <button>trigger events</button>
-        <div><div>placeholder for drop down menu</div><div>placeholder for key</div>
+        <div>        <SelectComponent
+            options={actions}
+            onChange={(item) => setSelectedOption(item)}
+            selectedKey={selectedOption}
+            placeholder={"type to search"}
+        /><input placeholder="key"></input>
         </div>
-        <div>{actions.map(act => 
+        <div>{actions.map(act =>
             <div className="hotkeylist-action">
                 <div>{act.action}</div>
                 <div>{act.hotkey}</div>
             </div>
         )}</div>
 
-        
+
     </div>)
 }
